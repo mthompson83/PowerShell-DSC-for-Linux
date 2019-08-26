9e47a8032c4572daa828a4ddf0b00515318e1a8c
Mike Thompson <mithomps@microsoft.com>
2019-08-26 10:51:27 -0400

    wip

---
+++ Providers/Modules/Plugins_UnitTests/Performance/plugin/Unit_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require 'test/unit'
+
+require_relative 'mock_oms_test.rb'
+
+require_relative 'in_perfmetrics_test.rb'
+require_relative 'PerfMetricDataCollector_test.rb'
+require_relative 'PerfMetricsEngine_test.rb'
