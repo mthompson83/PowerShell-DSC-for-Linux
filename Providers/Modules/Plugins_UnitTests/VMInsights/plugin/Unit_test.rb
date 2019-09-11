21cf4cf8f7a55099e10874a9602693b515b4848c
Mike Thompson <mithomps@microsoft.com>
2019-09-11 16:19:31 -0400

    renamed test files

--- Providers/Modules/Plugins_UnitTests/VMInsights/plugin/Unit_test.rb
+++ Providers/Modules/Plugins_UnitTests/VMInsights/plugin/Unit_test.rb
@@ -3,6 +3,6 @@ require 'test/unit'
 
 require_relative 'mock_oms_test.rb'
 
-require_relative 'in_perfmetrics_test.rb'
-require_relative 'PerfMetricDataCollector_test.rb'
-require_relative 'PerfMetricsEngine_test.rb'
+require_relative 'in_vminsights_test.rb'
+require_relative 'VMInsightsDataCollector_test.rb'
+require_relative 'VMInsightsEngine_test.rb'
